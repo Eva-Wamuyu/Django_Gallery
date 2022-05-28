68b1eb688ab520ca8b3ff20a24366c6b44e5b037
Eva <rutheve.eva@gmail.com>
2022-05-28 17:32:16 +0300

    change db to postgresql

--- Django_gallery/settings.py
+++ Django_gallery/settings.py
@@ -79,25 +79,25 @@ WSGI_APPLICATION = 'Django_gallery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': '',
-        'PASSWORD': '',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'dfbpm0oojms8o8',
-#         'USER': config('USER'),
-#         'PASSWORD': config('DB_PWD',),
-#         'HOST': config('HOST',)
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'USER': '',
+#         'PASSWORD': '',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DATABASE'),
+        'USER': config('USER'),
+        'PASSWORD': config('DB_PWD'),
+        'HOST': config('HOST'),
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
