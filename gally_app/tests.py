08bdbec086fe98d1dfae85666de69cd93b4fa662
Eva <rutheve.eva@gmail.com>
2022-05-28 17:53:59 +0300

    add tests and runtime.txt

--- gally_app/tests.py
+++ gally_app/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
+from .models import *
 # Create your tests here.
+
+class ImageTestCase(TestCase):
+
+
+
+class LocationTestCase(TestCase):
+
+
+class CategoryTestCase(TestCase):
+
+
+class CopyFunctionTestCase(TestCase):
+  
