fe41f1108efb79b4949baaa74c664192c3f81292
Eva <rutheve.eva@gmail.com>
2022-05-28 17:19:08 +0300

    register models

--- gally_app/admin.py
+++ gally_app/admin.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from gally_app import models
 class ImageAdmin(admin.ModelAdmin):
-  list_display = ("img","categoryName","locationName","description")
+  list_display = ("id","img","categoryName","locationName","description")
+
 
 
 
 admin.site.register(models.Location)
-admin.site.register(models.Image, models.ImageAdmin)
+admin.site.register(models.Image,ImageAdmin)
 admin.site.register(models.Category)
